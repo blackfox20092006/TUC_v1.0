02cfd6550a30c65677253545a15308f89a49a4a0
Hoàng Quang Nhân <81468391+blackfox20092006@users.noreply.github.com>
2022-11-07 23:13:15 +0700

    Update tuc.py

--- tuc.py
+++ tuc.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-import threading
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from colorama import init, AnsiToWin32
